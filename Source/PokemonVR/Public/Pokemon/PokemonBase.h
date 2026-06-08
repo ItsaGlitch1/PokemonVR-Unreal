899b437400ee875537063843787d67108448462f
John H <jharneyiiii@gmail.com>
2026-06-08 12:43:59 -0400

    cleaned up some pokemon data

--- Source/PokemonVR/Public/Pokemon/PokemonBase.h
+++ Source/PokemonVR/Public/Pokemon/PokemonBase.h
@@ -14,6 +14,12 @@ enum class EPokemonGender
 	Unknown, Male, Female
 };
 
+UENUM()
+enum class EPokerus
+{
+	None, Infected, Cured
+};
+
 USTRUCT()
 struct FNature
 {
@@ -45,6 +51,91 @@ struct FNatureArray
 	TArray<FNature> nature;
 };
 
+USTRUCT()
+struct FPokemonStats
+{
+	GENERATED_BODY()
+	UPROPERTY()
+	unsigned int hp;
+	UPROPERTY()
+	unsigned int atk;
+	UPROPERTY()
+	unsigned int def;
+	UPROPERTY()
+	unsigned int spa;
+	UPROPERTY()
+	unsigned int spd;
+	UPROPERTY()
+	unsigned int spe;
+};
+
+
+USTRUCT()
+struct FPokemon
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	unsigned int PID;
+
+	UPROPERTY()
+	bool isShiny;
+
+	UPROPERTY()
+	EPokemonGender gender;
+
+	//species (pokedex) //Type, Varients, Forms, base stats
+
+	UPROPERTY()
+	FString nickname;
+
+	UPROPERTY()
+	int exp;
+
+	UPROPERTY()
+	unsigned int level; //0-255
+
+	UPROPERTY()
+	FNature nature;
+
+	//held item
+
+	//ability
+
+	UPROPERTY()
+	bool isEgg;
+
+	UPROPERTY()
+	EPokerus pokerus;
+
+	//met location
+
+	//ball
+
+	UPROPERTY()
+	FDateTime metDate;
+
+	UPROPERTY()
+	unsigned int metLevel;
+
+	UPROPERTY()
+	FString originalTrainer;
+
+	UPROPERTY()
+	unsigned int friendship; //0-255
+
+	UPROPERTY()
+	FPokemonStats stats;
+
+	UPROPERTY()
+	FPokemonStats ivs; //0-31
+
+	UPROPERTY()
+	FPokemonStats evs; //0-255
+
+	//moves
+};
+
 UCLASS()
 class POKEMONVR_API APokemonBase : public ACharacter
 {
@@ -54,36 +145,7 @@ public:
 
 	FNatureArray* natureList;
 
-	UINT16 PID;
-	bool isShiny;
-	EPokemonGender gender;
-	//species (pokedex) //Type, Varients, Forms, base stats
-	FString nickname;
-
-	int exp;
-	UINT8 level; //0-255
-	FNature nature;
-	//held item
-	//ability
-
-	bool isEgg;
-	//pokerus
-
-	//met location
-	//ball
-	FDateTime metDate;
-	UINT8 metLevel;
-
-	FString originalTrainer;
-	UINT8 friendship; //0-255
-
-	UINT16 hp, atk, def, spa, spd, spe;
-
-	unsigned int hpIV : 5, atkIV : 5, defIV : 5, spaIV : 5, spdIV : 5, speIV : 5; //0-31
-
-	UINT8 hpEV, atkEV, defEV, spaEV, spdEV, speEV; //0-255
-
-	//moves
+	
 
 	// Sets default values for this character's properties
 	APokemonBase();
